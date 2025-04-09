b404d1ce3b68a2dfa97707d156e599f290571545
talkdambee <wotjr1806@gmail.com>
2025-04-10 01:23:18 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>담비(Dambee)에 오신 걸 환영합니다!</h1>
+      <p>이건 React로 만든 기본 구조입니다.</p>
+    </div>
+  );
+}
+
+export default App;
